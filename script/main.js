d774940cc889e983228f28f2348670faf8301208
IT20253912 <IT20253912@my.sliit.lk>
2023-02-08 22:12:49 +0530

    start

--- script/main.js
+++ script/main.js
@@ -1,6 +1,26 @@
 console.log('console test')
 
-let yname = prompt('Enter User value')
-let zname = confirm('Are you amila')
-console.log(yname)
-console.log(zname)
+// let yname = prompt('Enter User value')
+// let zname = confirm('Are you amila')
+// console.log(yname)
+// console.log(zname)
+
+let a = {
+    name: 'saman1',
+    age:23,
+}
+let b = {
+    name: 'saman2',
+    age:23,
+}
+let c = {
+    name: 'saman3',
+    age:23,
+}
+
+console.log({a,b,c})
+
+console.table({a,b,c})
+
+
+
