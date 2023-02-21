29467aa8d9feca17f1a05e9f5b8342e521f68324
IT20253912 <IT20253912@my.sliit.lk>
2023-02-22 00:34:08 +0530

    start day 3

---
+++ Scripts/main.js
@@ -0,0 +1,35 @@
+// let yname = confirm('Enter your name');
+
+// console.log(yname);
+
+// document.write('chathu')
+
+// let test = prompt('Enter name')
+
+// document.write(test)
+
+
+// console.warn('error test')
+
+let a = {
+    name : 'saman1',
+    age : 21,
+    city : 'colombo1',
+}
+let b = {
+    name : 'saman2',
+    age : 22,
+    city : 'colombo2',
+}
+let c = {
+    name : 'saman3',
+    age : 23,
+    city : 'colombo3',
+}
+
+
+// console.log(a) 
+// console.log(b) 
+// console.log(c) 
+
+console.table({a,b,c})
