b7522a938242c8747c7e76375c359003eb8866ab
IT20253912 <IT20253912@my.sliit.lk>
2023-02-22 09:08:06 +0530

    Day 4 start

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,25 +11,60 @@
 
 // console.warn('error test')
 
-let a = {
-    name : 'saman1',
-    age : 21,
-    city : 'colombo1',
-}
-let b = {
-    name : 'saman2',
-    age : 22,
-    city : 'colombo2',
-}
-let c = {
-    name : 'saman3',
-    age : 23,
-    city : 'colombo3',
-}
+// let a = {
+//     name : 'saman1',
+//     age : 21,
+//     city : 'colombo1',
+// }
+// let b = {
+//     name : 'saman2',
+//     age : 22,
+//     city : 'colombo2',
+// }
+// let c = {
+//     name : 'saman3',
+//     age : 23,
+//     city : 'colombo3',
+// }
 
 
 // console.log(a) 
 // console.log(b) 
 // console.log(c) 
 
-console.table({a,b,c})
+// console.table({a,b,c});
+
+// let a = 'Amila';
+// let b = 1234;
+// let c = true;
+// let d = 242n;
+// let e;
+// let f = null;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// console.log(typeof e);
+// console.log(typeof f);
+
+// b = String(b)
+// console.log(typeof b);
+
+
+// a = Number(a)
+// console.log(typeof a)
+
+// document.write(a)
+
+
+let a = 19;
+
+let b = 5;
+
+let name = 'saman'
+
+// console.log(b2)
+
+console.log(`your name is ${name} and age is ${a}`)
+
